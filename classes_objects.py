0edaf3b4a281a6da22dddfca38d770dd7c215772
Maqdad Ali <your-email@example.com>
2026-07-15 15:23:23 +0500

    The classes and object examples are being added

---
+++ classes_objects.py
@@ -0,0 +1,25 @@
+# Creating a Class
+class Student:
+
+    # Constructor (runs automatically when an object is created)
+    def __init__(self, name, age, department):
+        self.name = name
+        self.age = age
+        self.department = department
+
+    # Method to display student information
+    def display(self):
+        print("Student Information")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Department:", self.department)
+        print("---------------------------")
+
+
+# Creating Objects
+student1 = Student("Ali", 21, "Artificial Intelligence")
+student2 = Student("Ahmed", 22, "Computer Science")
+
+# Calling Methods using Objects
+student1.display()
+student2.display()
